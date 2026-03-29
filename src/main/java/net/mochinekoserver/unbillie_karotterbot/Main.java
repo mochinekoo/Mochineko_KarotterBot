ef027a6e4dab0060d92ebabe7934853a3ea77027
mochinekoo <mochinekoo0113@gmail.com>
2026-03-29 17:07:25 +0900

    [add] コメントを追加しました

--- src/main/java/net/mochinekoserver/unbillie_karotterbot/Main.java
+++ src/main/java/net/mochinekoserver/unbillie_karotterbot/Main.java
@@ -12,6 +12,14 @@ public class Main {
     public static final Logger BOT_LOGGER = Logger.getLogger("KarotterBot");
 
     public static void main(String[] args) {
+
+//        for (int n = 0; n < 100; n++) {
+//            String text = UnBillieManager.getRandomText();
+//            if (text.contains("アンビリバボー")) {
+//                System.out.println(text);
+//            }
+//        }
+
         BotManager botManager = BotManager.getInstance();
         initialize();
 
